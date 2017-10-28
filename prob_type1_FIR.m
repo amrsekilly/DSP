915f139ad74b0c6e42dce7a82eaa78f801838c5f
Amr Elsekilly <amr.e.dev@gmail.com>
2017-10-28 11:30:56 +0200

    added the type 1 FIR method

---
+++ prob_type1_FIR.m
@@ -0,0 +1,13 @@
+
+% Let h(n) = {?4, 1, ?1, ?2, 5, 6, 5, ?2, ?1, 1, ?4}. Determine the amplitude re- ?
+% sponse Hr (?) and the locations of the zeros of H (z).
+
+% we know that it's a type-1 FIR, cuz it's odd and symmetric about n
+
+% the filter
+h = [-4,1,-1,-2,5,6,5,-2,-1,1,-4];
+% get the number of samples of the filter (length of filter)
+M = length(h); n = 0:M-1;
+
+% use the type1 FIR function to define the filter
+[Hr,w,a,L] = Hr_Type1(h);
