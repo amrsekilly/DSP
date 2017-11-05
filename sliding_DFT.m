4b8ccafad64ff2647b0098f13f821d7aaf298b84
Amr Elsekilly <amr.e.dev@gmail.com>
2017-11-05 18:52:33 +0200

    still can't properly implement it

--- sliding_DFT.m
+++ sliding_DFT.m
@@ -15,7 +15,7 @@ x_new = [1 : 5];
 Xk_old = fft(x);
 
 % the sliding DFT equation
-Xk_new = exp(Xk_old + x_new(5) - x_new(5 - N)).^((2*pi*i)/N);
+Xk_new = exp(Xk_old + x_new(5) - x_new(5 - N)).^((2*pi*5*i)/N);
 Xk_sliding = Xk_new(4)
 
 x_fft = fft(x_new);
